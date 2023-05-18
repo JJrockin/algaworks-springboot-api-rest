ef318c6c736e9f96e7adf3f21727e4c2abbc8ece
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-05-18 18:12:17 -0300

    Adding Validation to data input

--- src/main/java/com/jjrockin/spring/dive/domain/model/Cliente.java
+++ src/main/java/com/jjrockin/spring/dive/domain/model/Cliente.java
@@ -6,6 +6,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -18,10 +21,19 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
 
     @Column(name = "fone")
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
 
