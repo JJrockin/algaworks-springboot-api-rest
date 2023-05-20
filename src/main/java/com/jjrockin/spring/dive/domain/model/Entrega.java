c92ae7ba7f25ad6cef6df6eb7f6309ad938e0bc1
Jeronimo Moraes Filho <99692252+JJrockin@users.noreply.github.com>
2023-05-20 15:35:49 -0300

    Replacing LocalDateTime for OffsetDateTime

--- src/main/java/com/jjrockin/spring/dive/domain/model/Entrega.java
+++ src/main/java/com/jjrockin/spring/dive/domain/model/Entrega.java
@@ -10,7 +10,8 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
@@ -40,8 +41,8 @@ public class Entrega {
     private StatusEntrega status;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private LocalDateTime dataPedido;
+    private OffsetDateTime dataPedido;
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private LocalDateTime dataFinalizacao;
+    private OffsetDateTime dataFinalizacao;
 
 }
